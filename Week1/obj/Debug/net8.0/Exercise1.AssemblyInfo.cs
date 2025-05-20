d9c1ab19d988c510f3dbd370e6f7efbdfb4990ff
MiguelPano99 <mpano@byupathway.edu>
2025-05-20 03:46:14 -0600

    main

--- Week1/obj/Debug/net8.0/Exercise1.AssemblyInfo.cs
+++ Week1/obj/Debug/net8.0/Exercise1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+062f9b1db16b9088cb2509aa878aad0388d51285")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb1077bd00f526a8919b76e2f1499f1452783c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
